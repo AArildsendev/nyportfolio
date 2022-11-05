f5647c527e3cbd4ee8ed9352c563cd74933da41c
Andreas Arildsen Jørgensen <arildsenjo@gmail.com>
2022-11-05 11:21:23 +0100

    Globale Scss variabler

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,38 @@
 import React from 'react';
 import { Navigation } from './Navigation/Navigation';
+import './globals/global.scss'
 
 
 
 function App() {
+
+  const[theme,setTheme] = React.useState("theme-default")
+  React.useEffect(()=>{},[theme])
+
+
+  const changeTheme = (theme : string) =>{
+    const newtheme = theme
+    if(newtheme === "theme-default"){
+      setTheme("theme-dark")
+
+    }else{
+      setTheme("theme-default")
+    }
+
+  }
+
+
+
   return (
-    <div className="App">
+    <div className={theme}>
       <header>
-      <Navigation/>
+        <Navigation />
       </header>
-
-    
       
-      
-  
-
+      <main className="section group">
+        <div className="col span_1_of_2"><h1>This is column 1</h1></div>
+        <div className="col span_1_of_2"><h1>This is column 2</h1></div>
+      </main>
     </div>
   );
 }
--- src/Navigation/Navigation.tsx
+++ src/Navigation/Navigation.tsx
@@ -1,6 +1,5 @@
 import React from 'react'
 import './style.scss'
-import '../index.css'
 
 export const Navigation = () => {
 
