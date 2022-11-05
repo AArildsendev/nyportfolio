982ae386dc81297be0b4ab8aaf8fecbf4dbd7c0a
Andreas Arildsen Jørgensen <arildsenjo@gmail.com>
2022-11-05 13:44:27 +0100

    asd

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,8 @@ import './globals/global.scss'
 
 
 
+
+
 function App() {
 
   const[theme,setTheme] = React.useState("theme-default")
@@ -30,8 +32,10 @@ function App() {
       </header>
       
       <main className="section group">
-        <div className="col span_1_of_2"><h1>This is column 1</h1></div>
-        <div className="col span_1_of_2"><h1>This is column 2</h1></div>
+        
+        <div className="col span_1_of_2"><h1 className='pp'>Velkommen til min Webudvikler Portfolio</h1></div>
+        <div className="col span_1_of_2"></div>
+       
       </main>
     </div>
   );
